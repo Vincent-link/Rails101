9d65ff6f5a2c6a2c42571b208b4d2dca14f55efd
liuhui <liuhuijisuanji@gmail.com>
2016-12-20 14:24:29 +0800

    use simple_format helper to decorate

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,5 @@
 class PostsController < ApplicationController
-
-  before_filter :authenticate_user!, :only => [:new, :create]
+  before_filter :authenticate_user!, :only => [:new, :create, :destroy]
 
   def new
     @group = Group.find(params[:group_id])
